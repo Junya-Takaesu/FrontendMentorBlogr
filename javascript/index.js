6e3ca901df36d3b34c6207ae6ce26a9ebcee2244
gandhi <ryokuchamugichauroncha.tea5@gmail.com>
2021-03-25 02:26:02 +0900

    Add dropdown list to navigation menu

---
+++ javascript/index.js
@@ -0,0 +1,4 @@
+import Navigation from "./navigation.js";
+
+const navigation = new Navigation();
+navigation.initialize();
---
+++ javascript/navigation.js
@@ -0,0 +1,52 @@
+export default class Navigation {
+
+  initialize() {
+    const navItems = document.querySelectorAll("#nav-left .nav-item");
+
+    navItems.forEach(navItem => {
+      navItem.addEventListener("click", () => {
+        this.toggleDropDownContent(navItem);
+      });
+    });
+
+    window.addEventListener("click", (event) =>{
+      if(!event.target.matches(".nav-link")) {
+        this.toggleDropDownContent();
+      }
+    });
+  }
+
+  toggleDropDownContent(navItem) {
+    let dropDownContent;
+    let arrowIcon;
+    if (navItem) {
+      let currentActiveDropdownList;
+      let currentActiveArrowIcon;
+
+      currentActiveDropdownList = document.querySelector(".show");
+      currentActiveArrowIcon = document.querySelector(".arrow-icon-expand");
+
+      if (currentActiveDropdownList) {
+        currentActiveDropdownList.classList.toggle("show");
+        currentActiveArrowIcon.classList.toggle("arrow-icon-expand");
+      } else {
+        arrowIcon = navItem.querySelector(".arrow-icon-default");
+        dropDownContent = navItem.querySelector(".dropdown-content");
+        dropDownContent.classList.toggle("show");
+      }
+    } else {
+      dropDownContent = document.querySelector(".show");
+      if (dropDownContent) {
+        arrowIcon = dropDownContent.previousElementSibling.querySelector(".arrow-icon-expand");
+        dropDownContent.classList.toggle("show");
+      }
+    }
+    if (arrowIcon) {
+      this.toggleArrowIcon(arrowIcon);
+    }
+  }
+
+  toggleArrowIcon(arrowIcon) {
+    arrowIcon.classList.toggle("arrow-icon-expand");
+  }
+}
